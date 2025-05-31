f651fa8cb3c75574404265f84e538aae99911a54
tcson62 <tcson62@gmail.com>
2025-05-31 10:21:25 -0600

    Files for checking

--- extract.py
+++ extract.py
@@ -30,25 +30,28 @@ def inference(image_path, prompt, sys_prompt="You are a helpful assistant.", max
     
 def extract_text(dir_path: str):
 
-    prompt = '''1. Extract all text from the image. Include equations and ensure you are extracting the entire text from all collumns. There may be text between many figures in the image, you must extract that text as well.
+  prompt = '''1. Extract all text from the image. Include equations and ensure you are extracting the entire text from all collumns. There may be text between many figures in the image, you must extract that text as well.
     2. Describe in detail any image, workflow, chart, graph for figure above a 'Fig.' caption. 
     3. Extract all contents from a table, keeping the structure of the table intact. 
     Output a response in JSON format with these keys: {"text": str, "figures":[{"fig": int, "caption": str, "description": str, "image": PIL image}], "tables":[{"table_no": int, "caption": str, "data": [][]}}
     For figures, also output a PIL formatted image of the figure you found.
     '''
-    responses = []
-    for file in os.listdir(dir_path):
-        filepath = os.path.join(dir_path, file)
-        if os.path.isfile(filepath):
-            id = re.split(r'\.', file)[0]
-            id = re.split(r'-', id)[-1]
-            response = inference(filepath, prompt)
-            output = response.replace('```', '')
-            output = output.replace('json', '')
-            output = output.replace(r'\n', '')
-            output = output.replace(r'\t', '')
-            output = output.replace('\\', '')
-            responses.append({"page":id, "response": json.loads(output)})
-        
-    with open("extracted_data.json", 'wb') as f:
-         json.dump(responses, codecs.getwriter('utf-8')(f), indent=2)
+  responses = []
+  for file in os.listdir(dir_path):
+    try: 
+      filepath = os.path.join(dir_path, file)
+      if os.path.isfile(filepath):
+        id = re.split(r'\.', file)[0]
+        id = re.split(r'-', id)[-1]
+        response = inference(filepath, prompt)
+        output = response.replace('```', '')
+        output = output.replace('json', '')
+        output = output.replace(r'\n', '')
+        output = output.replace(r'\t', '')
+        output = output.replace('\\', '')
+        responses.append({"page":id, "response": json.loads(output)})
+    except: 
+	    continue  
+
+  with open("extracted_data.json", 'wb') as f:
+    json.dump(responses, codecs.getwriter('utf-8')(f), indent=2)
